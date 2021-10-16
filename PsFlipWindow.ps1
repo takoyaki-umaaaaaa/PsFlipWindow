49a53c83a9d2e6c0db14712cd1bfd7d040d936fe
Takoyaki_umaaaaaa <takoyaki_umaaaaaa@hotmail.com>
2021-10-17 01:26:09 +0900

    初回アップロード

---
+++ PsFlipWindow.ps1
@@ -0,0 +1,59 @@
+﻿# 設定画面を起動
+# 保存先フォルダを設定した場合、User環境変数に保持する
+# (そのためregistryに内容が保持される)
+
+# Requires -Version 5.0
+
+Add-Type -AssemblyName system.windows.forms
+Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName system
+
+# 環境設定
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = "stop"						# エラーが発生した場合はスクリプトの実行を停止
+$PSDefaultParameterValues['out-file:width'] = 2000	# Script実行中は1行あたり2000文字設定
+
+Add-Type -AssemblyName system.windows.forms
+# 少しだけ今どきの Control表示にする
+[System.Windows.Forms.Application]::EnableVisualStyles()
+[System.Windows.Forms.Application]::VisualStyleState = 3
+
+# Window作成前に呼ぶことで、これ以降にこのScriptで作られる
+# Top Level windowが High DPI対応として動作する
+Add-Type -MemberDefinition @"
+[DllImport("user32.dll", SetLastError=true)]
+public static extern IntPtr SetThreadDpiAwarenessContext(IntPtr dpiContext);
+"@ -Namespace Win32 -Name NativeMethods
+[void][Win32.NativeMethods]::SetThreadDpiAwarenessContext(-4)
+
+# Dialog定義を読み込み
+#[xml]$xaml = Get-Content ($PSScriptRoot + "\PsFlipWindow.xaml")
+Import-Module -Name $PSScriptRoot\PsFlipWindow.psm1
+[xml]$xaml  = Xaml_FlipWindow
+$xamlReader = $xaml -as "System.Xml.XmlNodeReader"
+$flipWnd = [Windows.Markup.XamlReader]::Load( $xamlReader )
+
+
+# Control elementの objectを取得
+$baseWnd = $flipWnd.FindName( "basewindow" )
+$btnClose = $flipWnd.FindName( "button4" )
+
+# Event handler登録
+$baseWnd.Add_MouseLeftButtonDown({$baseWnd.DragMove() })
+$btnClose.Add_Click({ $flipWnd.Close() })
+
+<#
+$baseWnd.add_Loaded({
+	$hwnd = (New-Object System.Windows.Interop.WindowInteropHelper($this)).Handle
+	Write-Host "hwnd = $hwnd"
+})
+#>
+
+
+
+
+# Dialog表示 (Dialogの[閉じる]ボタン押下まで帰ってこない)
+[void]$flipWnd.showDialog()
+
+# 終了処理
+exit 0
---
+++ Readme.md
@@ -0,0 +1,48 @@
+# PowerShell で Flip Window
+
+## これは何か
+
+<img src=".\動作画面.png" alt="動作画面"  style="float: left; width: 35%; margin-right: 30px"/>Web siteでよく見かける、画面要素がカードのように回転する演出。あれが PowerShellでもできると聞き、その実現方法を確認したもの。
+(library や control として提供する目的のものではありません。ハードコードです；´Д｀)💨💨)
+
+裏と表、どちらにもボタンなどの Controlを配置可能で、もちろん押すこともできる(今回は[閉じる]以外は/ダミー)。ただし
+
+とにかく簡単に実現したかったのと、WPFはまだ勉強中のため、コード(というよりXamlの定義)は全くまとまっていない。
+
+<div style="clear: both;"></div>
+
+## 動作環境
+
+- PowerShell 5.1 (Windows 11標準のもの)
+- Windows 11
+- WPF (Xaml)による画面表示
+
+## 実行方法
+
+1. **ADS (ZoneID) を削除する**
+GitHubから downloadすると、Windowsが「Internet から取得したものです」とか言って実行を禁止してくる。非常に大きなお世話です。
+```powershell
+PS> Remove-Item PsFlipWindow-master.zip -Stream Zone.Identifier
+```
+2. **Zipを解凍する**
+好きな方法で。
+1. **Scriptを実行する**
+```powershell
+PS> powershell -ExecutionPolicy RemoteSigned -File ".\PsFlipWindow.ps1"
+```
+
+くるくる回る windowが出てくればOK。
+
+## 操作方法
+<dl>
+	<dt><strong>ウィンドウの移動</strong></dt>
+	<dd>ボタンなどの要素が無い場所をマウスドラッグ🖱️で移動</dd>
+	<dt><strong>終了方法</strong></dt>
+	<dd>[閉じる]ボタンを頑張って押す；´Д｀)</dd>
+</dl>
+
+## 参考リンク
+
+* [Animation **しない** Viewport3D のサンプル](https://docs.microsoft.com/en-us/dotnet/api/system.windows.media.media3d.viewport2dvisual3d?redirectedfrom=MSDN&view=windowsdesktop-5.0)(microsoft)
+* [WPF rotating button](https://stackoverflow.com/questions/7168420/wpf-rotating-button)(stack overflow)
+
